56550310101f39cb8345d8e59ba3a30cd964321d
tarq138 <36621806+tarq138@users.noreply.github.com>
2018-11-08 23:44:54 +0300

    add ProfilePhoto

--- mygram/User/admin.py
+++ mygram/User/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, ProfilePhoto
 
 admin.site.register(Profile)
+admin.site.register(ProfilePhoto)
 
 # Register your models here.
--- mygram/User/models.py
+++ mygram/User/models.py
@@ -10,6 +10,10 @@ class Profile(models.Model):
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -20,3 +24,19 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class ProfilePhoto(models.Model):
+    product = models.ForeignKey(Profile, verbose_name="Пользователь", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products_images/')
+    is_main = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s" % self.id
+
+    class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
