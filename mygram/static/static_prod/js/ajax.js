3ffb3f0503b0d22958ad8ba2996eb9eb3d7b5ea9
tarq138 <36621806+tarq138@users.noreply.github.com>
2018-12-01 19:33:07 +0300

    add frontend

---
+++ mygram/static/static_prod/js/ajax.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('#search').keyup(function(){
+        $.ajax({
+            type: "POST",
+            url: "search/",
+            data: {
+                'search_text' : $('#search').val(),
+                'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val()
+            },
+            success: searchSuccess,
+            dataType: 'html'
+        });
+    });
+});
+function searchSuccess(data, textStatus, jqXHR){
+    $('#search-result').html(data);
+}
---
+++ mygram/static/static_prod/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#txtSearch").autocomplete({
+        source: "/ajax_calls/search/",
+        minLength: 2,
+        open: function(){
+            setTimeout(function () {
+                $('.ui-autocomplete').css('z-index', 99);
+            }, 0);
+        }
+    });
+});
